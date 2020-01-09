4cd5a101c32a8f76a1a0fe4b1b5c3f93755a429f
joselanderosmrf <jose.landeros@marfeel.com>
2020-01-09 13:34:21 +0100

    fix(jest): disable max nested callbacks rule

--- packages/eslint-config-jest/index.js
+++ packages/eslint-config-jest/index.js
@@ -7,6 +7,7 @@ module.exports = {
 	},
 	extends: ['plugin:jest/recommended'],
 	rules: {
+		'max-nested-callbacks': 'off',
 		'jest/no-alias-methods': 'error',
 		'jest/no-jasmine-globals': 'error',
 		'jest/consistent-test-it': [
