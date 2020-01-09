09aaaeafc893b3524a8bc87cac6473139feeb3d0
joselanderosmrf <jose.landeros@marfeel.com>
2020-01-09 13:12:46 +0100

    fix: extend @marfeel/js config

--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -2,7 +2,7 @@ module.exports = {
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
 	extends: [
-		'../base/.eslintrc',
+		'@marfeel/js',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:@typescript-eslint/recommended-requiring-type-checking'
 	],
